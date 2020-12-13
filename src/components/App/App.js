88df762887456ef45d7a6ef89d133b4f2d6a73d9
zabalueva <age_eva@mail.ru>
2020-12-13 12:19:23 +0300

    Add moveBall to coord

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,62 +3,26 @@ import './App.css';
 import '../Gifts/Gift.css';
 
 import Circle from '../Circle';
-import Tree from '../Tree';
 
 import tree from '../Tree/tree.svg';
-import Coords from '../Coords/Coords';
-
-
-
-function useAwaitGit() {
-  let [name, setName] = useState('');
-  /* let [dateCreate, setDate] = useState(''); */
-
-  async function getDataGit() {
-    /*         let response = await fetch('/article/promise-chaining/user.json');
-            let user = await response.json(); */
-    let githubResponse = await fetch(`https://api.github.com/users/zabalueva`);
-    let githubUser = await githubResponse.json();
-    setName(githubUser.name);
-    /* setDate(githubUser.created_at); */
-  }
-  useEffect(() => {
-    getDataGit()
-  }, [])
-
-  return name
-}
-
-
 
 function App() {
-  let [top, setTop] = useState(12);
-  let [left, setLeft] = useState(1);
-  let [coordx, setX] = useState('');
-  let [coordy, setY] = useState('');
+  let [top, setTop] = useState(1);
+/*   let [left, setLeft] = useState(1); */
+  let [coordX, setX] = useState('');
+  let [coordY, setY] = useState('');
 
-/*   useEffect(() => {
+  useEffect(() => {
     console.log(top);
-  }, [top]) */
+  }, [top])
 
-  function moveDown() {
+  function moveBall(event) {
+    setX(event.pageX);
+    setY(event.pageY);
+  }
+
+ /*  function moveDown() {
     setTop(top + 50);
-  }
-
-  function moveRight() {
-    setLeft(left + 50);
-  }
-
-  /* function Coords(c) {
-    let element = c.target.getBoundingClientRect();
-    setX(() => {
-      coordx = element.left;
-    })
-    setY(() => {
-      coordy = element.top;
-      console.log(coordx, coordy);
-    })
-    return Circle
   } */
 
   return (
@@ -66,30 +30,20 @@ function App() {
       <div className="wrap">
         <Circle
           color={'red'}
-          position={{ top: top, left: left }}
+          position={{ top: coordY, left: coordX }}
         />
-        <Circle
+       {/*  <Circle
           color={'blue'}
           position={{ top: top + 50, left: left + 45 }}
-        />
-        <Tree
-          image={'url(' + tree + ')'}
-          color={'green'}
-          
-        />
-        <Coords 
-          color={'blue'}
-          top={50}
-          left={45 }
-        />
+        /> */}
+  
+        <div className="Tree">
+          <img src = {tree} alt = "XMastree" className="XMasTree" onClick={moveBall}></img>
+        </div>
       </div>
-      <button onClick={moveDown}>
+      {/* <button onClick={moveDown}>
         Двигать вниз
-      </button>
-
-      <button onClick={moveRight}>
-        Двигать вправо
-      </button>
+      </button> */}
 
     </div>
   );
--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -1,14 +1,13 @@
 import './Tree.css';
 import tree from '../Tree/tree.svg';
-import Circle from '../Circle';
-import Coords from '../Coords/Coords';
+
+
 
 function Tree (props) {
 
-
     return (
         <div className="Tree">
-            <img src = {tree} alt = "tree" className="XMasTree" onClick={Coords}></img>
+            <img src = {tree} alt = "tree" className="XMasTree"></img>
         </div>
     )
 }
