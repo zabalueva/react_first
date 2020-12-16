6afbc89f879e9c31dbc553cd7d1112d11d9d0855
zabalueva <age_eva@mail.ru>
2020-12-16 19:03:25 +0300

    Add second gift - meals

--- src/components/GiftVKInfo/GiftVKInfo.js
+++ src/components/GiftVKInfo/GiftVKInfo.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Gift1 from '../Gifts/Gift1.svg';
 import Gift2 from '../Gifts/Gift2.svg';
 import Gift3 from '../Gifts/Gift3.svg';
@@ -6,26 +6,32 @@ import './GiftVKInfo.css';
 
 function GiftVKInfo() {
     const [meal, setMeal] = useState(false);
-    /* const [avatar, setAvatar] = useState('');
-    const [days, setDays] = useState('');
+    const [imgMeal, setImgMeal] = useState('');
+    /* const [days, setDays] = useState('');
     const [login, setLogin] = useState(false); */
 
-    function giveData() {
-        
-        fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
-            .then(response => response.json())
-            .then(data => new Promise((resolve, reject) => {
+    async function giveData() {
+        const mealFind = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        const mealsy = await mealFind.json()
+        const mealsArrray = mealsy.meals;
+        const array = mealsArrray[0];
+        const titleMeal = array.strMeal;
+        const picMeal = array.strMealThumb;
 
-                setMeal(data.categories[0].strCategory);
+        setMeal(titleMeal);
+        setImgMeal(picMeal);
                 /* setAvatar(githubUserData.avatar_url);
 
                 let currentDate = new Date();
                 let formatDate = new Date(Date.parse(githubUserData.created_at));
                 const calc = ((currentDate - formatDate) / 1000 / 60 / 60 / 24);
                 setDays(calc.toFixed(0)); */
-                console.log(data.categories[0].strCategory)
-            }))
+        console.log(array);
+        console.log(titleMeal)
+        console.log(meal)
+            
     }
+    
 
     return (
         <div className="wrapGifts">
@@ -36,13 +42,12 @@ function GiftVKInfo() {
                 <img src={Gift3} alt="gift" className="Gift" onClick={giveData}></img>
             </div>
 
-            {/* {name &&
+            {meal &&
                 <div className="GiftGitDescr">
-                <p> Имя: {name} </p>
-                <p> Вы на github уже {days} дня! Это точно повод для праздника!</p>
-                <img src={avatar} alt={name} className='avatar'></img>
+                <p> На вашем праздничном столе обязательно должно быть: {meal} </p>
+                <img src={imgMeal} alt={meal} className='meal'></img>
                 </div>
-            } */}
+            }
 
         </div>
     );
