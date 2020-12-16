b213d5bd8a751e44039bdd8b74ff3d3d8aad3a56
zabalueva <age_eva@mail.ru>
2020-12-16 13:07:12 +0300

    Complete first gift

--- src/components/GiftGitInfo/GiftGitInfo.js
+++ src/components/GiftGitInfo/GiftGitInfo.js
@@ -3,7 +3,6 @@ import Gift1 from '../Gifts/Gift1.svg';
 import Gift2 from '../Gifts/Gift2.svg';
 import Gift3 from '../Gifts/Gift3.svg';
 import './GiftGitInfo.css';
-import GetCaptureGift1 from "../Gifts/Gifts"
 
 function GiftGitInfo() {
     const [name, setName] = useState(false);
@@ -23,26 +22,25 @@ function GiftGitInfo() {
                 let formatDate = new Date(Date.parse(githubUserData.created_at));
                 const calc = ((currentDate - formatDate) / 1000 / 60 / 60 / 24);
                 setDays(calc.toFixed(0));
-                
             }))
-        /* .then (GetCaptureGift1) */
-        
-       
     }
 
     return (
-        <div className="Gifts">
-            
-            <img src={Gift1} alt="gift" className="Gift" onClick={giveData}></img>
-            {name && <div>
+        <div className="wrapGifts">
+
+            <div className="Gifts">
+                <img src={Gift1} alt="gift" title="Подарок GitHub" className="Gift" onClick={giveData}></img>
+                <img src={Gift2} alt="gift" className="Gift" onClick={giveData}></img>
+                <img src={Gift3} alt="gift" className="Gift" onClick={giveData}></img>
+            </div>
+
+            {name &&
+                <div className="GiftGitDescr">
                 <p> Имя: {name} </p>
                 <p> Вы на github уже {days} дня! Это точно повод для праздника!</p>
                 <img src={avatar} alt={name} className='avatar'></img>
-            </div> }
-            <img src={Gift2} alt="gift" className="Gift" onClick={giveData}></img>
-            <img src = {Gift3} alt = "gift" className="Gift" onClick={giveData}></img>
-            
-           
+                </div>
+            }
 
         </div>
     );
