838c342136940a0973f4a3333061533b62130ba0
zabalueva <age_eva@mail.ru>
2020-12-21 18:43:25 +0300

    Fix addCircle (click=add)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,23 +6,15 @@ import Circle from '../Circle';
 import Gifts from '../Gifts';
 
 function App() {
-  const [coordX, setX] = useState('');
-  const [coordY, setY] = useState('');
   const [circles, setCircles] = useState([]);
 
-
   function addCircle(e) {
-    setX(e.pageX);
-    setY(e.pageY);
     setCircles(
       [...circles,
-      { coordX: coordX, coordY: coordY, id: circles.length }
+      { coordX: e.pageX, coordY: e.pageY, id: circles.length }
       ]
     );
   }
-
-
-
 
   return (
     <div className="App">
