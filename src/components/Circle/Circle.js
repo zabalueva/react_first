3613a0b3b4d9b6db19502f8610e8821314712cda
zabalueva <age_eva@mail.ru>
2021-01-05 15:31:48 +0300

    Add propTypes

--- src/components/Circle/Circle.js
+++ src/components/Circle/Circle.js
@@ -1,13 +1,17 @@
+import PropTypes from 'prop-types';
+
 import styles from './Circle.module.scss';
 
 function Circle(props) {
-    const { position } = props;
+    const { positionTop } = props;
+    const { positionLeft } = props;
     const colorArray = ['#f44336', '#e91e63', '#9c27b0',
                         '#673ab7', '#3f51b5', '#2196f3', 
                         '#03a9f4', '#00bcd4', '#CDDC39']
     const style = {
         backgroundColor: colorArray[Math.floor(Math.random() * colorArray.length)],
-        ...position
+        top: positionTop,
+        left: positionLeft
     }
     
     return (
@@ -16,4 +20,14 @@ function Circle(props) {
         
     )
 }
+Circle.propTypes = {
+    positionTop: PropTypes.number,
+    positionLeft: PropTypes.number
+};
+
+Circle.defaultProps = {
+    positionTop: 0,
+    positionLeft: 0
+};
+
 export default Circle;
